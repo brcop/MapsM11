8f119ba2746e60142ddba4232a7fa2000711b5c1
brcop <benjaminrdra@gmail.com>
2021-12-18 00:12:10 -0500

    Primer repositorio

---
+++ app/src/main/java/com/example/mapsm11/MapsActivity.kt
@@ -0,0 +1,70 @@
+package com.example.mapsm11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.mapsm11.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var map: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean{
+        val inflater=menuInflater
+        inflater.inflate(R.menu.options_map,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when
+      (item.itemId) {
+        R.id.normal_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_NORMAL
+            true
+        }
+        R.id.hybrid_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_HYBRID
+            true
+        }
+        R.id.satellite_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_SATELLITE
+            true
+        }
+        R.id.terrain_map -> {
+            map.mapType = GoogleMap.MAP_TYPE_TERRAIN
+            true
+        }
+        else -> super.onOptionsItemSelected(item)}
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+
+        val latitud=12.5200693
+        val longitud=-81.7307652
+        val homeLatLng= LatLng(latitud,longitud)
+        val zoomlevel= 15f
+        map.addMarker(MarkerOptions().position(homeLatLng).title("La Piscinita"))
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng,zoomlevel))
+    }
+
+}
